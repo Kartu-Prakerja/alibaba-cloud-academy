97419b46fc322e253b5938947d228d4b79b2c6a5
therexxar <therexxar@gmail.com>
2024-07-31 17:20:54 +0700

    remove credentials

--- js/cred.js
+++ js/cred.js
@@ -4,45 +4,45 @@
 /* exported handleSignoutClick */
 
 // TODO(developer): Set to client ID and API key from the Developer Console
-const CLIENT_ID = '362616290883-rctss7f688fdscjq55to07s74epiqfru.apps.googleusercontent.com'
-const API_KEY = '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';
-const SpreadsheetID = '1_8mE2TFaSjZwECW8UTe4swI0tjGHqHtXARDcbVCjEy4';
+// const CLIENT_ID = ''
+// const API_KEY = '';
+// const SpreadsheetID = '';
 
-// Discovery doc URL for APIs used by the quickstart
-const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
+// // Discovery doc URL for APIs used by the quickstart
+// const DISCOVERY_DOC = 'https://sheets.googleapis.com/$discovery/rest?version=v4';
 
-// Authorization scopes required by the API; multiple scopes can be
-// included, separated by spaces.
-const SCOPES = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/spreadsheets'];
+// // Authorization scopes required by the API; multiple scopes can be
+// // included, separated by spaces.
+// const SCOPES = ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.file', 'https://www.googleapis.com/auth/spreadsheets'];
 
-let tokenClient;
-let gapiInited = false;
-let gisInited = false;
+// let tokenClient;
+// let gapiInited = false;
+// let gisInited = false;
 
-/**
- * Callback after api.js is loaded.
- */
-function gapiLoaded() {
-    gapi.load('client', initializeGapiClient);
-}
+// /**
+//  * Callback after api.js is loaded.
+//  */
+// function gapiLoaded() {
+//     gapi.load('client', initializeGapiClient);
+// }
 
-function gisLoaded() {
-    tokenClient = google.accounts.oauth2.initTokenClient({
-      client_id: CLIENT_ID,
-      scope: SCOPES,
-      callback: '', // defined later
-    });
-    gisInited = true;
-  }
+// function gisLoaded() {
+//     tokenClient = google.accounts.oauth2.initTokenClient({
+//       client_id: CLIENT_ID,
+//       scope: SCOPES,
+//       callback: '', // defined later
+//     });
+//     gisInited = true;
+//   }
 
 
-async function initializeGapiClient() {
-    gapi.client.init({
-        apiKey: API_KEY,
-        discoveryDocs: [DISCOVERY_DOC],
-    });
-    gapiInited = true;
-}
+// async function initializeGapiClient() {
+//     gapi.client.init({
+//         apiKey: API_KEY,
+//         discoveryDocs: [DISCOVERY_DOC],
+//     });
+//     gapiInited = true;
+// }
 
 // function appendValues(spreadsheetId, range, valueInputOption, _values, callback) {
 //     let values = [
